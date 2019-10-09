6d89357a2762f5777764e6c0631e970f27f93907
andrewjknell <andrewjknell@yahoo.com>
2019-10-08 18:58:01 -0700

    first commit

---
+++ python_practice/basic_loops.py
@@ -0,0 +1,31 @@
+# for x in range(151):
+#     print(x)
+
+# for x in range(0,1000,5):
+#     print(x)
+
+# for x in range(100):
+#     if (x %10 != 0) and (x %5 == 0):
+#         print('dojo')
+#     if (x %5 == 0) and (x %10 == 0):
+#         print('coding dojo')
+#     else:
+#         print(x)
+
+# sum = 0
+# for x in range(1,5000,2):
+#     sum = sum + x
+# print(sum)
+
+# for x in range(2018,0,-4):
+#     print(x)
+
+# x = 2
+# y = 29
+# z = 4
+# for x in range(x,y):
+#     if x %z == 0:
+#         print(x)
+
+
+
---
+++ python_practice/practice.py
@@ -0,0 +1,5 @@
+x = 10
+if x > 50:
+    print("bigger than 50")
+else:
+    print("smaller than 50")
